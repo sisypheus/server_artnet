ad1272f933384267d4c5fce9b4f851e2fcee0ab6
Theo <theo.poette@epitech.eu>
2021-09-22 18:51:06 +0200

    fix: cors route

--- index.js
+++ index.js
@@ -13,8 +13,34 @@ const fastify = Fastify();
 const algolia = algoliasearch(process.env.app_id, process.env.api_key);
 const index = algolia.initIndex('user_search');
 
-fastify.register(cors, {
-  origin: true,
+// fastify.register(cors, {
+//   origin: true,
+// });
+
+fastify.register((fastify, options, done) => {
+  fastify.register((cors), {
+    origin: "*",
+    methods: ["POST"]
+  }),
+  fastify.route({
+    method: "POST",
+    url: "/create/user",
+    handler: async (req, res) => {
+      store.updateUserData(req.body).then((ref) => {
+        const objectID = ref.id;
+        const toIndex = req.body;
+        index.saveObject({
+          objectID,
+          ...toIndex,
+        }).catch(err => console.log(err));
+        res.code(200);
+      }).catch(err => {
+        console.log(err);
+        res.code(500).send(err);
+      });
+    }
+  });
+  done();
 });
 
 class FirestoreClient {
@@ -46,23 +72,20 @@ class FirestoreClient {
 const store = new FirestoreClient();
 //fastify routes//
 
-fastify.post('/create/user', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'POST');
-  store.updateUserData(req.body).then((ref) => {
-    const objectID = ref.id;
-    const toIndex = req.body;
-    index.saveObject({
-      objectID,
-      ...toIndex,
-    }).catch(err => console.log(err));
-    res.code(200);
-  }).catch(err => {
-    console.log(err);
-    res.code(500).send(err);
-  });
-});
+// fastify.post('/create/user', async (req, res) => {
+//   store.updateUserData(req.body).then((ref) => {
+//     const objectID = ref.id;
+//     const toIndex = req.body;
+//     index.saveObject({
+//       objectID,
+//       ...toIndex,
+//     }).catch(err => console.log(err));
+//     res.code(200);
+//   }).catch(err => {
+//     console.log(err);
+//     res.code(500).send(err);
+//   });
+// });
 
 fastify.delete('/delete/user/', async (req, res) => {
   const doc = store.db.doc(req.body.user);

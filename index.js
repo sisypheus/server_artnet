f045a50afd6716aff125b6c36e9979de11295da6
Theo <theo.poette@epitech.eu>
2021-09-22 20:40:31 +0200

    fix: api endpoint

--- index.js
+++ index.js
@@ -105,5 +105,5 @@ fastify.get('/', () => {
 });
 
 fastify.listen(process.env.PORT || 3000, () => {
-  console.log(`server listening on port 3000`);
+  console.log(`server listening on port ${process.env.PORT || 3000}`);
 });
